1927e0e4f4a4a474768cc990456ee30abb2b1e3b
vdiez <diez@outlook.com>
2020-09-28 18:06:43 +0200

    added list method updated aws sdk

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let path = require('path');
 let fs = require('fs-extra');
 
-function load_protocol(protocol) {
+function list() {
     return fs.readdir(path.posix.join(__dirname, './protocols'))
+}
+function load_protocol(protocol) {
+    return list()
         .then(files => {
             if (!files.includes(protocol + ".js")) throw "Incorrect protocol";
             return require('./protocols/' + protocol);
@@ -18,8 +21,16 @@ module.exports = ({logger, protocol, params} = {}) => {
         if (typeof logger.debug === "function") log.debug = logger.debug;
         if (typeof logger.verbose === "function") log.verbose = logger.verbose;
     }
-    return load_protocol(protocol).then(client => client(params, log));
+    return load_protocol(protocol).then(client => new client(params, log));
 }
 module.exports.parameters = protocol => load_protocol(protocol).then(client => client.parameters)
+
+module.exports.list = () => list()
+    .then(protocols => protocols.reduce((result, filename) => {
+        let extension = path.posix.extname(filename);
+        filename = filename.slice(0, -extension.length);
+        result[filename] = require('./protocols/' + filename);
+        return result;
+    }, {}));
 
 module.exports.get_class = protocol => load_protocol(protocol)
