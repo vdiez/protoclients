836396f46ea7b323b89d8bbaebbc15051ede1a63
vdiez <diez@outlook.com>
2020-09-29 22:32:27 +0200

    index does not return promise any more

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 let path = require('path');
 let fs = require('fs-extra');
-let protocols = [];
-
-function load() {
-    return fs.readdir(path.posix.join(__dirname, './protocols')).then(files => protocols = files);
-}
-function load_protocol(protocol) {
-    if (!protocols.includes(protocol + ".js")) throw "Incorrect protocol or module not initialized";
-    return require('./protocols/' + protocol);
-}
+let protocols = fs.readdirSync(path.posix.join(__dirname, './protocols'));
 
 module.exports = ({logger, protocol, params} = {}) => {
     let log = {info() {}, warn() {}, error() {}, verbose() {}, debug() {}};
@@ -19,11 +11,13 @@ module.exports = ({logger, protocol, params} = {}) => {
         if (typeof logger.debug === "function") log.debug = logger.debug;
         if (typeof logger.verbose === "function") log.verbose = logger.verbose;
     }
-    return load_protocol(protocol).then(client => new client(params, log));
+    if (!protocols.includes(protocol + ".js")) throw "Incorrect protocol";
+    return new (require('./protocols/' + protocol))(params, log);
 }
-module.exports.parameters = protocol => load_protocol(protocol).then(client => client.parameters)
-
-module.exports.load_protocols = load;
+module.exports.parameters = protocol => {
+    if (!protocols.includes(protocol + ".js")) throw "Incorrect protocol";
+    return require('./protocols/' + protocol).parameters();
+}
 
 module.exports.list = () => protocols.reduce((result, protocol) => {
     let extension = path.posix.extname(protocol);
@@ -32,4 +26,7 @@ module.exports.list = () => protocols.reduce((result, protocol) => {
     return result;
 }, {});
 
-module.exports.get_class = protocol => load_protocol(protocol)
+module.exports.get_class = protocol => {
+    if (!protocols.includes(protocol + ".js")) throw "Incorrect protocol";
+    return require('./protocols/' + protocol);
+}
